95a7e837338ddd352b8ad68142a67929f6753ed4
ikennot <domdomkenneth23@gmail.com>
2025-11-27 14:25:06 +0800

    create for showing categories

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -1,11 +1,45 @@
 <?php
 
 namespace App\Http\Controllers\API;
-
+use  App\Http\Resources\CategoryResource;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\CategoryRequest;
+use App\Services\CategoryService;
 
-class CategoryController
+class CategoryController extends Controller
 {
-    //
+
+   protected $categoryService;
+    public function __construct(CategoryService $categoryService)
+    {
+         $this->categoryService = $categoryService;
+    }
+
+
+    public function index(){
+
+        $result = $this->categoryService->getAll();
+
+
+
+        return response()->json([
+            'success' => true,
+            'data' => CategoryResource::collection($result)
+        ]);
+
+
+    }
+
+    public function store(CategoryRequest $request)
+    {
+        $result = $this->categoryService->create($request->validated());
+
+        return response()->json([
+            'success' => true,
+            'data' => new CategoryResource($result)
+        ], 201);
+    }
+
+
+
 }
