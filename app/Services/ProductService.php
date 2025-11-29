19631edce459bdce448d0b1f464f0d390015e2a0
ikennot <domdomkenneth23@gmail.com>
2025-11-29 16:43:11 +0800

    done with api get pagination

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -9,9 +9,11 @@ class ProductService{
     $query = Product::query();
     
     if ($search) 
-     $query->where('name','Ilike',"%{$search}%")->orWhere('description','Ilike',"%{$search}%");
+     $query->where('name','Ilike',"%{$search}%")->orWhere('description','Ilike',"%{$search}%")->orWhere('sku','Ilike',"%{$search}%");
     else if($filter)
-    $query->where('category','Ilike',"%{$filter}%");
+ $query->join('categories', 'categories.id', '=', 'products.category_id')->join('brands','brands.id','=','products.brand_id')
+      ->where('categories.name', 'ILIKE', "%{$filter}%")->orwhere('brands.name','ILike','%{%filter}%');
+
     
 
       return $query->paginate(10)->withQueryString();
