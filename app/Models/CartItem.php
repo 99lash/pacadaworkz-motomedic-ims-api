efe6064aa777db78a74145528ba1d590d76fe340
Asher <asherjohn48@gmail.com>
2025-12-07 22:52:43 +0800

    feat: create Cart and CartItem models

---
+++ app/Models/CartItem.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class CartItem extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'cart_id',
+        'product_id',
+        'quantity',
+        // 'unit_price'
+    ];
+
+    public function cart(): BelongsTo
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /* Nagdadalawang isip lagyan ng 'unit_price' column hahahha */
+    // public function getSubtotalAttribute() {
+    // return $this->quantity * $this->unit_price;
+    // }
+}
