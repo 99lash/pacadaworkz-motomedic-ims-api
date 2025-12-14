ae0929aefb5983ba56c3c25ffabc09f7a5b5aff0
Asher <asherjohn48@gmail.com>
2025-12-14 16:41:14 +0800

    feat: add CartItemNotFoundException and EmptyCartException for cart error handling

---
+++ app/Exceptions/POS/Cart/CartItemNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exceptions\POS\Cart;
+
+use Exception;
+
+class CartItemNotFoundException extends Exception
+{
+    protected $message = 'Cart item not found';
+    protected $code = 404;
+}
