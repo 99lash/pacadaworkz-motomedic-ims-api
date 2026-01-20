4d8eb1e253c16c4748b5b5436d0bf6d1de4d5d32
ikennot <domdomkenneth23@gmail.com>
2026-01-20 20:39:47 +0800

    add test permissions

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test-permissions', function () {
+    try {
+        \File::put(storage_path('test.txt'), 'ok');
+        return 'Storage writable!';
+    } catch (\Exception $e) {
+        return 'Error: '.$e->getMessage();
+    }
+});
+
+
 Route::get('/api/dummy', [DummyController::class, 'dummy']);
