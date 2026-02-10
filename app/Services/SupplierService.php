feebcb5ef40509f774de06804492136804f5ea74
ikennot <domdomkenneth23@gmail.com>
2026-02-10 12:41:29 +0800

    done modifying supplierService

--- app/Services/SupplierService.php
+++ app/Services/SupplierService.php
@@ -2,9 +2,17 @@
 namespace App\Services;
 
 use App\Models\Supplier;
+use App\Services\ActivityLogService;
+use Illuminate\Support\Facades\Auth;
 
 class SupplierService
 {
+    protected $activityLogService;
+
+    public function __construct(ActivityLogService $activityLogService)
+    {
+        $this->activityLogService = $activityLogService;
+    }
     public function getAllSuppliers($search = null)
     {
         $query = Supplier::query();
@@ -23,19 +31,25 @@ class SupplierService
 
     public function createSupplier(array $data)
     {
-        return Supplier::create($data);
+        $supplier = Supplier::create($data);
+        $this->activityLogService->log('Supplier', 'Created', 'Created supplier: ' . $supplier->name, Auth::id());
+        return $supplier;
     }
 
     public function updateSupplier(array $data, $id)
     {
         $supplier = Supplier::findOrFail($id);
+        $oldData = $supplier->toArray();
+        $supplier_name = $supplier->name;
         $supplier->update($data);
+        $this->activityLogService->log('Supplier', 'Updated', "Update supplier info #{$supplier_name}", Auth::id());
         return $supplier;
     }
 
     public function deleteSupplier($id)
     {
         $supplier = Supplier::findOrFail($id);
+        $this->activityLogService->log('Supplier', 'Deleted', 'Deleted supplier: ' . $supplier->name, Auth::id());
         $supplier->delete();
     }
 }
